94e9a27c39ec881a58920c6395be41fd639dbdaf
jacoblee93 <jacoblee93@gmail.com>
2023-09-17 23:33:45 -0700

    Fix hook

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -102,7 +102,7 @@ export function ChatWindow(props: {
       });
       setIsLoading(false);
     }
-  });
+  }, []);
 
   async function embedPDF (e: FormEvent<HTMLFormElement>) {
     console.log(e);
