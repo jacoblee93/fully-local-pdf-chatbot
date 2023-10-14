80351e739ed5bb44b2a2f519347cb1e5e6406a08
Beckett <133655553+severian42@users.noreply.github.com>
2023-10-14 18:23:58 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Fully Client-Side Chat Over Documents</title>
+        <title>ANIMA: Chat with your PDF</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <meta
           name="description"
@@ -21,7 +21,7 @@ export default function RootLayout({
         />
         <meta
           property="og:title"
-          content="Fully Client-Side Chat Over Documents"
+          content="Fully Local Chat Over Documents"
         />
         <meta
           property="og:description"
@@ -30,17 +30,10 @@ export default function RootLayout({
         <meta property="og:image" content="/images/og-image.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Fully Client-Side Chat Over Documents"
-        />
-        <meta
-          name="twitter:description"
-          content="Upload a PDF, then ask questions about it - without a single remote request!"
-        />
-        <meta name="twitter:image" content="/images/og-image.png" />
-      </head>
+       
+        
+    
+       
       <body className={publicSans.className}>
         <div className="flex flex-col p-4 md:p-12 h-[100vh]">{children}</div>
       </body>
